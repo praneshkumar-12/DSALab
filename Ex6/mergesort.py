bb110a53da8df6f9a80721a3508dc29bf083afe7
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-10 11:41:55 +0545

    Added functionality to count number of comparisons

--- Ex6/mergesort.py
+++ Ex6/mergesort.py
@@ -1,9 +1,13 @@
+count = 0
+
 def merge(seq1, seq2):
+    global count
     i = j = 0
     m = len(seq1)
     n = len(seq2)
     result = []
     while (i<m and j<n):
+        count += 1
         if seq1[i] < seq2[j]:
             result.append(seq1[i])
             i += 1
@@ -26,11 +30,13 @@ def mergesort(seq):
         mid = length // 2
         return merge(mergesort(seq[:mid]), mergesort(seq[mid:]))
 
+
 if __name__ == "__main__":
-    import random
+    import random 
     sizes = [1, 10, 50, 100, 500, 1000, 5000, 10000]
     for size in sizes:
         myseq = [random.randint(-10000, 10000) for _ in range(size)]
-        print(myseq)
-        print(mergesort(myseq))
+        mergesort(myseq)
+        print("="*50)
+        print(f"Size: {size} \n Number of comparisons: {count}")
         print("="*50)
