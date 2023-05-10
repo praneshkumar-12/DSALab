cf9a8f8cc8acbbe9f537fc74386b5ed9ead71a84
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-10 12:48:02 +0545

    added functionality for binary search

---
+++ Ex6/binarysearch.py
@@ -0,0 +1,33 @@
+"""
+This module provides functionality for finding the search element in a sequence by using binary search
+
+Original Author: Pranesh Kumar
+
+Created on 10 May 2023
+
+"""
+
+def binarysearch(seq, low, high, sval):
+    """
+    This function finds the search element in a sequence recursively using binary search
+    """
+
+    if low > high: # base condition
+        return -1
+    else:
+        mid = (low + high) // 2
+        if sval == seq[mid]:
+            return mid
+        elif sval < seq[mid]:
+            return binarysearch(seq, low, mid - 1, sval)
+        else:
+            return binarysearch(seq, mid + 1, high, sval)
+        
+if __name__ == "__main__":
+    import random
+    size = 10
+    myseq = [random.randint(-10000, 10000) for _ in range(size)]
+    print(sorted(myseq))
+    search = random.choice(myseq)
+    print(search)
+    print(binarysearch(sorted(myseq), 0, size, search))
