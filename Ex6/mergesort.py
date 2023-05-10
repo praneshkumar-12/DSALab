281e3332b83c3246b94cf992f7da88150a726061
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-10 11:47:55 +0545

    Added functionality for best case ands worst case

--- Ex6/mergesort.py
+++ Ex6/mergesort.py
@@ -1,3 +1,5 @@
+import random
+
 count = 0
 
 def merge(seq1, seq2):
@@ -30,13 +32,35 @@ def mergesort(seq):
         mid = length // 2
         return merge(mergesort(seq[:mid]), mergesort(seq[mid:]))
 
+def bestcasecomplexity(size):
+    myseq = [random.randint(-10000, 10000) for _ in range(size)]
+    myseq.sort()
+    mergesort(myseq)
+
+def worstcasecomplexity(size):
+    myseq = [random.randint(-10000, 10000) for _ in range(size)]
+    myseq.sort(reverse=True)
+    mergesort(myseq)
+
 
 if __name__ == "__main__":
-    import random 
     sizes = [1, 10, 50, 100, 500, 1000, 5000, 10000]
     for size in sizes:
+        count = 0
         myseq = [random.randint(-10000, 10000) for _ in range(size)]
         mergesort(myseq)
         print("="*50)
-        print(f"Size: {size} \n Number of comparisons: {count}")
+        print(f"Size: {size} \nNumber of comparisons: {count}")
         print("="*50)
+
+    count = 0
+    bestcasecomplexity(10)
+    print("="*50)
+    print(f"Best Case - Size: {10} \nNumber of comparisons: {count}")
+    print("="*50)
+
+    count = 0
+    worstcasecomplexity(10)
+    print("="*50)
+    print(f"Worst Case - Size: {10} \nNumber of comparisons: {count}")
+    print("="*50)
