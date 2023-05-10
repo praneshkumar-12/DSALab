0265fec77694d4361044bfd74816ba4bf75d7c7b
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-10 12:18:32 +0545

    Added comments and docstrings

--- Ex6/mergesort.py
+++ Ex6/mergesort.py
@@ -1,3 +1,16 @@
+"""
+    This module provides functionality for checking the execution time and 
+    number of comparisons for sorting of various sequences using merge sort
+
+    This output can be used for ratio analysis of algorithms by taking n as the size of the array or
+    as the number of comparisons.
+
+    Original Author: Pranesh Kumar
+
+    Created on 10 May 2023
+"""
+
+# importing the necessary modules
 import random
 import timeit
 
@@ -5,6 +18,17 @@ count = 0
 starttime = 0
 
 def merge(seq1, seq2):
+    """
+    This function performs merge operation on the two lists which are passsed to the function.
+
+    Args:
+        seq1 (Iterable): First sequence to be merged
+        seq2 (Iterable): Second sequence to be merged
+
+    Returns:
+        Merged list of seq1 and seq2 which is sorted
+    """
+
     global count
     i = j = 0
     m = len(seq1)
@@ -27,24 +51,39 @@ def merge(seq1, seq2):
     return result
 
 def mergesort(seq):
+    """
+    Performs recursive merge operation on list by using divide and conquer technique
+    
+    Args:
+        seq (Iterable) Sequene to be sorted
+    
+    Returns:
+        Sorted sequence
+    """
     length = len(seq)
-    if (length < 2):
+    if (length < 2): # Base Condition
         return (seq[:])
     else:
         mid = length // 2
-        return merge(mergesort(seq[:mid]), mergesort(seq[mid:]))
+        return merge(mergesort(seq[:mid]), mergesort(seq[mid:])) # Recursive call
 
 def bestcasecomplexity(size):
+    """
+    Finds out the best case complexity of merge sort by passing an already sorted list
+    """
     myseq = [random.randint(-10000, 10000) for _ in range(size)]
     myseq.sort()
     mergesort(myseq)
 
 def worstcasecomplexity(size):
+    """
+    Finds out the worst case complexity of merge sort by passing an already sorted list in descending order
+    """
     myseq = [random.randint(-10000, 10000) for _ in range(size)]
     myseq.sort(reverse=True)
     mergesort(myseq)
 
-
+# driver code
 if __name__ == "__main__":
     f1 = open("merge-sort-comp.txt", "w")
     f2 = open("merge-sort-time.txt", "w")
