f2d6c3f7bc9b4e954a27f97bb37acc2d9bc66b0f
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-16 10:02:14 +0530

    Added imports and provided proper spacing

--- Ex6/binarysearch.py
+++ Ex6/binarysearch.py
@@ -7,12 +7,15 @@ Created on 10 May 2023
 
 """
 
+from mergesort import mergesort
+
+
 def binarysearch(seq, low, high, sval):
     """
     This function finds the search element in a sequence recursively using binary search
     """
 
-    if low > high: # base condition
+    if low > high:  # base condition
         return -1
     else:
         mid = (low + high) // 2
@@ -22,12 +25,14 @@ def binarysearch(seq, low, high, sval):
             return binarysearch(seq, low, mid - 1, sval)
         else:
             return binarysearch(seq, mid + 1, high, sval)
-        
+
+
 if __name__ == "__main__":
     import random
+
     size = 10
     myseq = [random.randint(-10000, 10000) for _ in range(size)]
-    print(sorted(myseq))
+    mergesort(myseq)
     search = random.choice(myseq)
     print(search)
     print(binarysearch(sorted(myseq), 0, size, search))
