784b77630438668eb62414a23f3d23e865f930b8
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-20 11:00:25 +0530

    formatted using pep8

--- Ex6/mergesort.py
+++ Ex6/mergesort.py
@@ -17,6 +17,7 @@ import timeit
 count = 0
 starttime = 0
 
+
 def merge(seq1, seq2):
     """
     This function performs merge operation on the two lists which are passsed to the function.
@@ -34,7 +35,7 @@ def merge(seq1, seq2):
     m = len(seq1)
     n = len(seq2)
     result = []
-    while (i<m and j<n):
+    while i < m and j < n:
         count += 1
         if seq1[i] < seq2[j]:
             result.append(seq1[i])
@@ -42,13 +43,14 @@ def merge(seq1, seq2):
         else:
             result.append(seq2[j])
             j += 1
-    while (i<m):
+    while (i < m):
         result.append(seq1[i])
         i += 1
-    while (j<n):
+    while (j < n):
         result.append(seq2[j])
         j += 1
     return result
+
 
 def mergesort(seq):
     """
@@ -61,11 +63,12 @@ def mergesort(seq):
         Sorted sequence
     """
     length = len(seq)
-    if (length < 2): # Base Condition
-        return (seq[:])
+    if length < 2:  # Base Condition
+        return seq[:]
     else:
         mid = length // 2
-        return merge(mergesort(seq[:mid]), mergesort(seq[mid:])) # Recursive call
+        return merge(mergesort(seq[:mid]), mergesort(seq[mid:]))  # Recursive call
+
 
 def bestcasecomplexity(size):
     """
@@ -75,6 +78,7 @@ def bestcasecomplexity(size):
     myseq.sort()
     mergesort(myseq)
 
+
 def worstcasecomplexity(size):
     """
     Finds out the worst case complexity of merge sort by passing an already sorted list in descending order
@@ -82,6 +86,7 @@ def worstcasecomplexity(size):
     myseq = [random.randint(-10000, 10000) for _ in range(size)]
     myseq.sort(reverse=True)
     mergesort(myseq)
+
 
 # driver code
 if __name__ == "__main__":
@@ -94,39 +99,38 @@ if __name__ == "__main__":
         starttime = timeit.default_timer()
         myseq = [random.randint(-10000, 10000) for _ in range(size)]
         mergesort(myseq)
-        print("="*50, file = f1)
-        print(f"Size: {size} \nNumber of comparisons: {count}", file = f1)
-        print("="*50, file = f1)
+        print("=" * 50, file=f1)
+        print(f"Size: {size} \nNumber of comparisons: {count}", file=f1)
+        print("=" * 50, file=f1)
 
-        exectime = timeit.default_timer() - starttime        
-        print("="*50, file = f2)
-        print(f"Size: {size} \nExecution Time: {exectime}", file = f2)
-        print("="*50, file = f2)
+        exectime = timeit.default_timer() - starttime
+        print("=" * 50, file=f2)
+        print(f"Size: {size} \nExecution Time: {exectime}", file=f2)
+        print("=" * 50, file=f2)
 
     count = 0
     starttime = timeit.default_timer()
     bestcasecomplexity(10)
-    print("="*50, file = f1)
-    print(f"Best Case - Size: {10} \nNumber of comparisons: {count}", file = f1)
-    print("="*50, file = f1)
+    print("=" * 50, file=f1)
+    print(f"Best Case - Size: {10} \nNumber of comparisons: {count}", file=f1)
+    print("=" * 50, file=f1)
 
-    exectime = timeit.default_timer() - starttime        
-    print("="*50, file = f2)
-    print(f"Best Case - Size: {10} \nExecution Time: {exectime}", file = f2)
-    print("="*50, file = f2)
+    exectime = timeit.default_timer() - starttime
+    print("=" * 50, file=f2)
+    print(f"Best Case - Size: {10} \nExecution Time: {exectime}", file=f2)
+    print("=" * 50, file=f2)
 
     count = 0
     starttime = timeit.default_timer()
     worstcasecomplexity(10)
-    print("="*50, file = f1)
-    print(f"Worst Case - Size: {10} \nNumber of comparisons: {count}", file = f1)
-    print("="*50, file = f1)
+    print("=" * 50, file=f1)
+    print(f"Worst Case - Size: {10} \nNumber of comparisons: {count}", file=f1)
+    print("=" * 50, file=f1)
 
-    exectime = timeit.default_timer() - starttime        
-    print("="*50, file = f2)
-    print(f"Worst Case - Size: {10} \nExecution Time: {exectime}", file = f2)
-    print("="*50, file = f2)
-
+    exectime = timeit.default_timer() - starttime
+    print("=" * 50, file=f2)
+    print(f"Worst Case - Size: {10} \nExecution Time: {exectime}", file=f2)
+    print("=" * 50, file=f2)
 
     f1.close()
     f2.close()
