ffe2656fca54a22e8425a9f87eaead99a30de6e9
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-20 11:00:12 +0530

    added comaprison count

--- Ex6/binarysearch.py
+++ Ex6/binarysearch.py
@@ -9,19 +9,23 @@ Created on 10 May 2023
 
 from mergesort import mergesort
 
+count = 0
 
 def binarysearch(seq, low, high, sval):
+    global count
     """
     This function finds the search element in a sequence recursively using binary search
     """
-
+    count += 1
     if low > high:  # base condition
         return -1
     else:
         mid = (low + high) // 2
+        count += 1
         if sval == seq[mid]:
             return mid
-        elif sval < seq[mid]:
+        count += 1
+        if sval < seq[mid]:
             return binarysearch(seq, low, mid - 1, sval)
         else:
             return binarysearch(seq, mid + 1, high, sval)
@@ -30,9 +34,10 @@ def binarysearch(seq, low, high, sval):
 if __name__ == "__main__":
     import random
 
-    size = 10
+    size = int(input("Enter the size of the list: "))
     myseq = [random.randint(-10000, 10000) for _ in range(size)]
     search = random.choice(myseq)
     print(search)
     print(mergesort(myseq))
     print(binarysearch(mergesort(myseq), 0, size, search))
+    print(size, count)
