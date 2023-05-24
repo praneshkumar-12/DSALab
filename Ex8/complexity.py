94cbf1c61c0ffd28b6c429904edc50eec5f26f7d
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-24 12:17:10 +0530

    added complexity.py

---
+++ Ex8/complexity.py
@@ -0,0 +1,13 @@
+from dynamicarray import DynamicArray
+from timeit import default_timer
+
+if __name__ == "__main__":
+    d1 = DynamicArray(30000)
+    elts = [1, 5, 10, 100, 500, 1000, 5000, 10_000, 50_000, 1_00_000]
+    for elt in elts:
+        number_of_elements_to_append = elt
+        start = default_timer()
+        for _ in range(number_of_elements_to_append):
+            d1.append(None)
+        stop = default_timer()
+        print(f"Size: {number_of_elements_to_append}; Time taken: {(stop - start) / number_of_elements_to_append}")
