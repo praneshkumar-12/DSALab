7c14b4fe93a1793495fbfbbe03ca6038be9f0fba
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-25 17:24:16 +0530

    Added docstrings and other functions and its testing

--- Ex8/dynamicarray.py
+++ Ex8/dynamicarray.py
@@ -136,11 +136,113 @@ class DynamicArray:
             raise IndexError("Index out of range!")
         self.array[idx] = value
 
+    def insert(self, idx, elt):
+        """
+            Inserts an element at a given index in the array.
+
+            Args:
+                idx: The index to insert the element at.
+                elt: The element to insert
+
+            Raises:
+                IndexError: If the index is out of range
+        """
+        if not 0 <= idx <= self.n:
+            raise IndexError("Index out of range!")
+        if self.n == self.capacity:
+            self.resize(2 * self.capacity)
+        for i in range(self.n, idx, -1):
+            self.array[i] = self.array[i - 1]
+        self.array[idx] = elt
+        self.n += 1
+
+    def delete(self, idx):
+        """
+            Deletes an element at a given index in the array.
+
+            Args:
+                idx: The index of the element to delete.
+
+            Raises:
+                IndexError: If the index is out of range
+        """
+        if not 0 <= idx < self.n:
+            raise IndexError("Index out of range!")
+        for i in range(idx, self.n - 1):
+            self.array[i] = self.array[i + 1]
+        self.n -= 1
+        if self.n < self.capacity // 4:  # if the size of the array is smaller than 25%, then shrink the array
+            self.resize(self.capacity // 2)
+
+    def extend(self, lst):
+        """
+            Extends a list of elements in the array
+            Args:
+                lst: list of elements to be extended
+        """
+        for elt in lst:
+            self.append(elt)
+
+    def __contains__(self, search_elt):
+        """
+            Checks if an element is present in the dynamic array
+            Args:
+                search_elt: The element to be searched in the array
+
+            Returns:
+                bool: True, if the search element is present in the array, otherwise False.
+        """
+        for idx in range(0, self.n):
+            if self.array[idx] == search_elt:
+                return True
+        return False
+
+    def index(self, elt):
+        """
+            Checks if an element is present in the array and returns the index of the element
+            Args:
+                elt: Element whose index is to be found out
+
+            Returns:
+                idx (int): If the element is found, index of the element is returned, else -1.
+        """
+        for idx in range(0, self.n):
+            if self.array[idx] == elt:
+                return idx
+        return -1
+
+    def count(self, count_elt):
+        """
+            Returns the number of occurrences of an element in the dynamic array
+            Args:
+                count_elt: Element whose number of occurrences is to be found
+
+            Returns:
+                count (idx): The number of occurrences of an element in the dynamic array
+        """
+        count = 0
+        for idx in range(0, self.n):
+            if self.array[idx] == count_elt:
+                count += 1
+        return count
+
 
 # driver code
 if __name__ == "__main__":
-    d1 = DynamicArray(4)
-    d1.append(324)
+    import random
+
+    d1 = DynamicArray(10)
+    for i in range(5):
+        d1.append(random.randint(0, 100))
     print(d1)
-    d1[0] = 5
+    d1.insert(2, -5)
     print(d1)
+    d1.insert(6, 25)
+    print(d1)
+    d1.delete(6)
+    print(d1)
+    d1.extend([1, 2, 3, 4, 5, 6, 4])
+    print(d1)
+    print(2 in d1)
+    print(d1.index(5))
+    print(d1.count(4))
