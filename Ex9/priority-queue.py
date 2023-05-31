076ef08d8b0390d82270fd2f14877c595ae2d55b
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-05-31 20:11:17 +0530

    Added docstrings

--- Ex9/priority-queue.py
+++ Ex9/priority-queue.py
@@ -8,6 +8,13 @@ class EmptyQueueError(Exception):
 
 class PQueue:
     def __init__(self, hp_size, lp_size):
+        """
+        Initialize a PQueue object with separate arrays for high priority and low priority queues.
+
+        Args:
+            hp_size (int): Size of the high priority queue.
+            lp_size (int): Size of the low priority queue.
+        """
         self.lp_btm = 0
         self.hp_btm = 0
         self.hp_top = 0
@@ -18,10 +25,26 @@ class PQueue:
         self.lp_queue = self.make_array(lp_size)
 
     def make_array(self, size):
+        """
+        Create and return a new array of the specified size.
+
+        Args:
+            size (int): Size of the array.
+
+        Returns:
+            ctypes.Array: A new array of the specified size.
+        """
         temp = (size * ctypes.py_object)()
         return temp
 
     def resize(self, queue_num, size):
+        """
+        Resize the specified queue to the given size.
+
+        Args:
+            queue_num (int): Queue number (0 for high priority, 1 for low priority).
+            size (int): New size of the queue.
+        """
         if queue_num == 0:
             new_queue = self.make_array(size)
             for i in range(self.hp_top):
@@ -36,6 +59,15 @@ class PQueue:
             self.lp_cap = size
 
     def is_empty(self, queue_num):
+        """
+        Check if the specified queue is empty.
+
+        Args:
+            queue_num (int): Queue number (0 for high priority, 1 for low priority).
+
+        Returns:
+            bool: True if the queue is empty, False otherwise.
+        """
         if queue_num == 0:
             if len(self.hp_queue) == 0:
                 return True
@@ -48,6 +80,15 @@ class PQueue:
                 return self.check_all_dash(1)
 
     def check_all_dash(self, queue_num):
+        """
+        Check if all elements in the specified queue are dashes ("-").
+
+        Args:
+            queue_num (int): Queue number (0 for high priority, 1 for low priority).
+
+        Returns:
+            bool: True if all elements in the queue are dashes, False otherwise.
+        """
         if queue_num == 0:
             f = False
             for i in range(self.hp_top):
@@ -66,9 +107,21 @@ class PQueue:
             return f
 
     def __len__(self):
+        """
+        Return the total number of elements in the PQueue.
+
+        Returns:
+            int: Total number of elements in the PQueue.
+        """
         return len(self.hp_queue)
 
     def __str__(self):
+        """
+        Return a string representation of the PQueue.
+
+        Returns:
+            str: String representation of the PQueue.
+        """
         queue_as_string = "HPQ:["
         for idx in range(self.hp_top):
             queue_as_string += str(self.hp_queue[idx])
@@ -83,16 +136,35 @@ class PQueue:
         return queue_as_string
 
     def peek(self, queue_num):
+        """
+        Return the element at the front of the specified queue without removing it.
+
+        Args:
+            queue_num (int): Queue number (0 for high priority, 1 for low priority).
+
+        Returns:
+            object: The element at the front of the queue.
+
+        Raises:
+            EmptyQueueError: If the specified queue is empty.
+        """
         if queue_num == 0:
             if self.is_empty(0):
                 raise EmptyQueueError("High Priority Queue is empty!")
             return self.hp_queue[0]
         elif queue_num == 1:
             if self.is_empty(1):
-                raise EmptyQueueError("High Priority Queue is empty!")
+                raise EmptyQueueError("Low Priority Queue is empty!")
             return self.lp_queue[0]
 
     def enqueue(self, queue_num, elt):
+        """
+        Enqueue the element to the specified queue.
+
+        Args:
+            queue_num (int): Queue number (0 for high priority, 1 for low priority).
+            elt (object): Element to be enqueued.
+        """
         if queue_num == 0:
             if self.hp_top == self.hp_cap:
                 self.resize(0, 2 * self.hp_cap)
@@ -105,6 +177,16 @@ class PQueue:
             self.lp_top += 1
 
     def dequeue(self):
+        """
+        Dequeue and return the element from the high priority queue.
+        If the high priority queue is empty, dequeue from the low priority queue.
+
+        Returns:
+            object: The dequeued element.
+
+        Raises:
+            EmptyQueueError: If both high priority and low priority queues are empty.
+        """
         if not self.is_empty(0):
             del_elt = self.hp_queue[self.hp_btm]
             self.hp_queue[self.hp_btm] = "-"
@@ -112,7 +194,7 @@ class PQueue:
             return del_elt
         else:
             if self.is_empty(1):
-                return
+                raise EmptyQueueError("Both High Priority and Low Priority Queues are empty!")
             del_elt = self.lp_queue[self.lp_btm]
             self.lp_queue[self.lp_btm] = "-"
             self.lp_btm += 1
