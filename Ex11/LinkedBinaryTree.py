80dd36ac555bec908d5e63f96c3b969f0c3822dc
praneshkumar-12 <me.praneshkumar@gmail.com>
2023-06-28 11:26:01 +0530

    added docstring for LinkedBinaryTree.py

--- Ex11/LinkedBinaryTree.py
+++ Ex11/LinkedBinaryTree.py
@@ -39,6 +39,18 @@ class LinkedBinaryTree(AbstractBinaryTree):
                 t_right.root = None
 
     def addRoot(self, item):
+        """
+        Adds a root node with the given item to the tree.
+
+        Args:
+            item: The item to be stored in the root node.
+
+        Returns:
+            The root position of the added node.
+
+        Raises:
+            ValueError: If the root already exists.
+        """
         if self.root is not None:
             raise ValueError("Root already exists")
         else:
@@ -47,30 +59,97 @@ class LinkedBinaryTree(AbstractBinaryTree):
             return self.root
 
     def __len__(self):
+        """
+        Returns the number of nodes in the tree.
+
+        Returns:
+            The size of the tree.
+        """
         return self.size
 
     def getParent(self, pos):
+        """
+        Returns the parent position of the given position 'pos'.
+
+        Args:
+            pos: The position to get the parent of.
+
+        Returns:
+            The parent position of 'pos'.
+        """
         return pos.parent
 
     def getLeft(self, pos):
+        """
+        Returns the left child position of the given position 'pos'.
+
+        Args:
+            pos: The position to get the left child of.
+
+        Returns:
+            The left child position of 'pos'.
+        """
         return pos.left
 
     def getRight(self, pos):
+        """
+        Returns the right child position of the given position 'pos'.
+
+        Args:
+            pos: The position to get the right child of.
+
+        Returns:
+            The right child position of 'pos'.
+        """
         return pos.right
 
     def getRoot(self):
+        """
+        Returns the root position of the tree.
+
+        Returns:
+            The root position.
+        """
         return self.root
 
     def getSize(self):
+        """
+        Returns the number of nodes in the tree.
+
+        Returns:
+            The size of the tree.
+        """
         return self.size
 
     def getNum_children(self, pos):
+        """
+        Returns the number of children of the given position 'pos'.
+
+        Args:
+            pos: The position to get the number of children of.
+
+        Returns:
+            The number of children of 'pos'.
+        """
         if pos is None:
             return 0
         else:
             return 1 + self.getNum_children(pos.left) + self.getNum_children(pos.right)
 
     def addLeft(self, item, pos=None):
+        """
+        Adds a left child node with the given item to the specified position 'pos' or the root if 'pos' is None.
+
+        Args:
+            item: The item to be stored in the left child node.
+            pos: The position to add the left child to. If None, the left child is added to the root.
+
+        Returns:
+            The position of the added left child node.
+
+        Raises:
+            ValueError: If the left child already exists.
+        """
         if pos is None:
             pos = self.root
         if self.getLeft(pos) is not None:
@@ -81,6 +160,19 @@ class LinkedBinaryTree(AbstractBinaryTree):
             return pos.left
 
     def addRight(self, item, pos=None):
+        """
+        Adds a right child node with the given item to the specified position 'pos' or the root if 'pos' is None.
+
+        Args:
+            item: The item to be stored in the right child node.
+            pos: The position to add the right child to. If None, the right child is added to the root.
+
+        Returns:
+            The position of the added right child node.
+
+        Raises:
+            ValueError: If the right child already exists.
+        """
         if pos is None:
             pos = self.root
         if self.getRight(pos) is not None:
@@ -91,6 +183,12 @@ class LinkedBinaryTree(AbstractBinaryTree):
             return pos.right
 
     def preorder(self, pos):
+        """
+        Performs a preorder traversal starting from the given position 'pos'.
+
+        Args:
+            pos: The starting position for the preorder traversal.
+        """
         self.string += str(pos.item) + ","
         if pos.left is not None:
             self.preorder(pos.left)
@@ -98,6 +196,12 @@ class LinkedBinaryTree(AbstractBinaryTree):
             self.preorder(pos.right)
 
     def postorder(self, pos):
+        """
+        Performs a postorder traversal starting from the given position 'pos'.
+
+        Args:
+            pos: The starting position for the postorder traversal.
+        """
         if pos.left is not None:
             self.postorder(pos.left)
         if pos.right is not None:
@@ -105,6 +209,12 @@ class LinkedBinaryTree(AbstractBinaryTree):
         self.string += str(pos.item) + ","
 
     def inorder(self, pos):
+        """
+        Performs an inorder traversal starting from the given position 'pos'.
+
+        Args:
+            pos: The starting position for the inorder traversal.
+        """
         if pos.left is not None:
             self.inorder(pos.left)
         self.string += str(pos.item) + ","
@@ -112,6 +222,12 @@ class LinkedBinaryTree(AbstractBinaryTree):
             self.inorder(pos.right)
 
     def __str__(self):
+        """
+        Returns a string representation of the tree by performing preorder, inorder, and postorder traversals.
+
+        Returns:
+            A string representation of the tree.
+        """
         self.string = "Preorder: "
         self.preorder(self.root)
         self.string += "|Inorder: "
